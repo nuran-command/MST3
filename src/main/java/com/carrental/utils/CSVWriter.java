87af59e8bbd466a0bbe164603d2c7f1480b2a8ad
Nurdan Zhanabaev <nurdanzanabaev@gmail.com>
2025-10-22 18:42:03 +0500

    corrected some errors

--- src/main/java/com/carrental/utils/CSVWriter.java
+++ src/main/java/com/carrental/utils/CSVWriter.java
@@ -5,6 +5,7 @@ import com.carrental.model.MSTResult;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
+import java.util.Locale;
 
 public class CSVWriter {
     public static void writeResults(String filePath, List<Graph> graphs,
@@ -18,20 +19,21 @@ public class CSVWriter {
                 MSTResult prim = primResults.get(i);
                 MSTResult kruskal = kruskalResults.get(i);
 
-                // Comparison
                 String cheaper = prim.getTotalCost() < kruskal.getTotalCost() ? "Prim" :
                         prim.getTotalCost() > kruskal.getTotalCost() ? "Kruskal" : "Equal";
                 String faster = prim.getExecutionTimeMs() < kruskal.getExecutionTimeMs() ? "Prim" :
                         prim.getExecutionTimeMs() > kruskal.getExecutionTimeMs() ? "Kruskal" : "Equal";
 
-                // Prim
-                writer.write(String.format("%d,%d,%d,%b,Prim,%.2f,%d,%.3f,%s,%s\n",
+                // Prim result line
+                writer.write(String.format(Locale.US,
+                        "%d,%d,%d,%b,Prim,%.2f,%d,%.3f,%s,%s\n",
                         g.getId(), g.vertexCount(), g.edgeCount(), prim.isConnected(),
                         prim.getTotalCost(), prim.getOperationsCount(), prim.getExecutionTimeMs(),
                         cheaper, faster));
 
-                // Kruskal
-                writer.write(String.format("%d,%d,%d,%b,Kruskal,%.2f,%d,%.3f,%s,%s\n",
+                // Kruskal result line
+                writer.write(String.format(Locale.US,
+                        "%d,%d,%d,%b,Kruskal,%.2f,%d,%.3f,%s,%s\n",
                         g.getId(), g.vertexCount(), g.edgeCount(), kruskal.isConnected(),
                         kruskal.getTotalCost(), kruskal.getOperationsCount(), kruskal.getExecutionTimeMs(),
                         cheaper, faster));
